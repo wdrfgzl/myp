d9e9cb364520bd1aa69f72c40bc348309d85fd1b
admin <253813442@QQ.com>
2018-12-17 21:13:33 +0800

    第一次提交

---
+++ HelloProjet/src/Main.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("idea---git测试");
+        System.out.println("idea---git测试");
+        System.out.println("idea---git测试");
+        System.out.println("idea---git测试");
+    }
+
+
+    @Test
+    public void testHelloWrold() {
+        System.out.println("HelloWorld!");
+    }
+}
